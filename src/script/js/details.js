3a5aa0479079238c0214500ae5d6204bbcfb6680
luwenya <1273662862@qq.com>
2018-10-07 22:57:11 +0800

    五个页面的静态效果基本完成！

--- src/script/js/details.js
+++ src/script/js/details.js
@@ -1,7 +1,6 @@
 //1.导入模块的公用部分
 !function($){
-	$('.reg_head').load('header.html .regist_header');
+	$('.details_header').load('header.html .h_header_wrap');
 	//第一个参数：地址。
 	//第二个参数：选择器。
-	$('.reg_foot').load('footer.html .simplefooter');
 }(jQuery);
